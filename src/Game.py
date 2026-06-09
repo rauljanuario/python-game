1144db2b8caf6c6d8b5ceb7d9f2280b811c3f8d6
Raul Januario <raul.s.januario@gmail.com>
2026-06-09 09:41:19 -0300

    feat(game): add defeat condition

--- src/Game.py
+++ src/Game.py
@@ -4,7 +4,7 @@ import sys
 
 import pygame
 
-from src.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from src.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION, C_RED, C_WHITE
 from src.Level import Level
 from src.Menu import Menu
 from src.Score import Score
@@ -30,6 +30,10 @@ class Game:
                     level_return = level.run(player_score)
                     if level_return:
                         score.save(menu_return, player_score)
+                
+                if not level_return:
+                    self.show_game_over_screen()
+
             elif menu_return == MENU_OPTION[1] or menu_return == MENU_OPTION[2]:
                 pass
             elif menu_return == MENU_OPTION[3]:
@@ -40,3 +44,32 @@ class Game:
             else:
                 pygame.quit()
                 sys.exit()
+
+    def show_game_over_screen(self):
+        
+        pygame.mixer_music.load('./assets/menu.ogg')
+        pygame.mixer_music.play(-1)
+        
+        self.window.fill((0, 0, 0)) 
+        
+        font_title = pygame.font.SysFont('Arial', 60, bold=True)
+        font_text = pygame.font.SysFont('Arial', 24)
+      
+        title_surf = font_title.render('GAME OVER', True, C_RED) 
+        text_surf = font_text.render('Press any key to return to the menu.', True, C_WHITE) 
+        
+        title_rect = title_surf.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2 - 30))
+        text_rect = text_surf.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2 + 40))
+        
+        self.window.blit(title_surf, title_rect)
+        self.window.blit(text_surf, text_rect)
+        pygame.display.flip()
+        
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN: 
+                    waiting = False
